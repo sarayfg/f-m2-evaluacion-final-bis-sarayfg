c9b94f549daec10226660295387842da41be3b58
sarayfg <sarayfernandezgarcia@gmail.com>
2019-05-03 15:39:19 +0200

    save number in LS

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -10,23 +10,25 @@ let cardNumber = '';
 let url = '';
 let img = '';
 
+getSavedCards();
 //BUCLE PARA INCLUIR UN LISTENER EN CADA INPUT
-for (const input of inputs) {
-  input.addEventListener ('click', handlerInputClick);
+for (const inputEl of inputs) {
+  inputEl.addEventListener ('input', handlerInputClick);
 }
 
 // FUNCION DEL CLICK DEL INPUT QUE RECOGE SU VALOR Y CREA LA URL
 function handlerInputClick (event) {
   cardNumber = event.currentTarget.value;
   url = `https://raw.githubusercontent.com/Adalab/cards-data/master/${cardNumber}.json`;
+  saveCardsNumber ();
 }
 
 btnEl.addEventListener ('click', handlerBtnClick);
 
 // FUNCION HANDLER BUTTON QUE RECOGE EL FETCH
 function handlerBtnClick () {
-  cardsList.innerHTML ='';
-  backCardsList.innerHTML ='';
+  cardsList.innerHTML = '';
+  backCardsList.innerHTML = '';
   getCards ();
 }
 
@@ -40,7 +42,7 @@ function getCards () {
       for (let i = 0; i < data.length; i++) {
         img = data[i].image;
         printCards (img);
-        printBackCards();
+        printBackCards ();
       }
     });
 }
@@ -51,9 +53,9 @@ function getCards () {
 
 function printCards (imagen) {
   const newItem = document.createElement ('li');
-  newItem.setAttribute('class', 'pokemon-card');
+  newItem.setAttribute ('class', 'pokemon-card');
   const newImage = document.createElement ('img');
-  newImage.setAttribute('class', 'pokemon-img');
+  newImage.setAttribute ('class', 'pokemon-img');
   newImage.src = `${imagen}`;
   newItem.appendChild (newImage);
   cardsList.appendChild (newItem);
@@ -61,9 +63,31 @@ function printCards (imagen) {
 
 function printBackCards () {
   const newBackCard = document.createElement ('li');
-  newBackCard.setAttribute('class', 'back-card');
+  newBackCard.setAttribute ('class', 'back-card');
   const newImgBackCard = document.createElement ('img');
   newImgBackCard.src = backCardImg;
-  newBackCard.appendChild(newImgBackCard);
-  backCardsList.appendChild(newBackCard);
+  newBackCard.appendChild (newImgBackCard);
+  backCardsList.appendChild (newBackCard);
 }
+
+//local storage
+
+function saveCardsNumber () {
+  localStorage.setItem ('number', cardNumber);
+}
+
+function getSavedCards() {
+  if (localStorage.number) {
+    const savedCards = localStorage.getItem('number');
+    console.log(savedCards);
+    for (const input of inputs) {
+        if(input.value === savedCards) {
+            input.setAttribute('checked', 'checked');
+        }
+    }
+}
+}
+
+//hacer funcion que al hacer click en la tarjeta, me quite la clase hidden de la imagen de pokemon y me la añada a la otra
+
+//otre opcion seria : quitame este elemento de la lista y añademe este
