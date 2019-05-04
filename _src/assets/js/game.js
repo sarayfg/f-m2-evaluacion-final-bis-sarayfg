8e29082a9b1b77996b93ea8e670be908762871f2
sarayfg <sarayfernandezgarcia@gmail.com>
2019-05-04 15:53:17 +0200

    get card id

---
+++ _src/assets/js/game.js
@@ -0,0 +1,14 @@
+/* eslint-disable strict */
+
+// cuando pincho en dos cartas y estas son iguales:
+    // ver en el data si comparten algun dato, como por ejemplo un id
+     //si estas son iguales se quedaran las dos volteadas, (o las borramos de la lista?)
+     //si son diferentes todas se darán la vuelta
+
+// el elemento que es igual es el .pair del objeto
+
+function checkCard(event) {
+    if (event.currentTarget.cardId !== event.target.cardId) {
+        event.currentTarget.classList.add('hidden');
+    }
+}
